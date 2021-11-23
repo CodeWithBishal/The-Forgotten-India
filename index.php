44f8467b921e9d427fc2293a71bd100275ef0397
Bishal (git) <codewithbishal@gmail.com>
2021-11-23 16:35:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/style.css?v=1">
-    <link rel="apple-touch-icon" sizes="180x180" href="https://codewithbishal.com/logo/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="https://codewithbishal.com/logo/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="https://codewithbishal.com/logo/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://codewithbishal.com/static/logo/logo-180x180.svg">
+    <link rel="icon" type="image/svg" sizes="32x32" href="https://codewithbishal.com/static/logo/logo-32x32.svg">
+    <link rel="icon" type="image/svg" sizes="16x16" href="https://codewithbishal.com/static/logo/logo-16x16.svg">
     <title>The Forgotten India</title>
 </head>
 
